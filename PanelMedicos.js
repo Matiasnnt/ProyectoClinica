970bbfabc560acdb2c198d00ee918fdd0a4a6bc3
Matias Navarro Torres <matiasnavarrot@gmail.com>
2023-12-27 18:45:30 -0300

    agregando js del panel de medicos

--- PanelMedicos.js
+++ PanelMedicos.js
@@ -1,26 +1,21 @@
+// Mostrar el turno seleccionado
+// Obtener el elemento de la tabla
+let tabla = document.getElementById('turnosPacientesBody');
 
-    // Función para cargar los turnos de pacientes en la tabla
-    function cargarTurnosPacientes(turnos) {
-        const turnosPacientesBody = document.getElementById('turnosPacientesBody');
-        turnosPacientesBody.innerHTML = '';
+// Obtener los datos de localStorage
+let datos = JSON.parse(localStorage.getItem('datos'));
 
-        //reucpera los turnos de localstorage
-        let turnos = localStorage.getItem('datos')
-        if (turnos) {
-            //convierte los turnos en un array
-            turnos = JSON.parse(turnos)
-
-        }
-
-        turnos.forEach(turno => {
-            const fila = document.createElement('tr');
-            fila.innerHTML = `
-                <td>${turno.paciente}</td>
-                <td>${turno.fecha}</td>
-                <td>${turno.hora}</td>
-            `;
-            turnosPacientesBody.appendChild(fila);
-        });
-    }
-
-    
+// Verificar si hay datos
+if (datos) {
+    // Crear las filas de la tabla
+    let fila = `<tr>
+                    <td>${datos.medicos}</td>
+                    <td>${datos.especialidad}</td>
+                    <td>${datos.hora}</td>
+                </tr>`;
+    // Agrega la fila a la tabla
+    tabla.innerHTML = fila;
+} else {
+    // Muestra un mensaje si no hay datos
+    tabla.innerHTML = '<tr><td colspan="3">No hay datos disponibles.</td></tr>';
+}
